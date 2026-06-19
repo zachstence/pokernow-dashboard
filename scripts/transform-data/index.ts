a27fc0e55e7863605153b134fcd5ed509e95dd29
Zachary Stence <zachstence@gmail.com>
2026-06-19 16:04:55 -0500

    feat: loadPlayersFile

--- scripts/transform-data/index.ts
+++ scripts/transform-data/index.ts
@@ -1,4 +1,8 @@
 import { loadHandsFile } from './loadHandsFile';
+import { loadPlayersFile } from './loadPlayersFile';
+
+const players = await loadPlayersFile();
+console.log({ players });
 
 const handsFile = await loadHandsFile('./data/poker-now-hands-game-pglXMnDTq51E3NjE7DsBSAhVj.json');
 console.log({ handsFile });
---
+++ scripts/transform-data/loadPlayersFile.ts
@@ -0,0 +1,19 @@
+import { readFile } from 'node:fs/promises';
+import z from 'zod';
+
+const PlayersFileSchema = z.array(
+	z.object({
+		displayName: z.string(),
+		color: z.string(),
+		playerIds: z.array(z.string())
+	})
+);
+
+export type PlayersFile = z.infer<typeof PlayersFileSchema>;
+
+export const loadPlayersFile = async (): Promise<PlayersFile> => {
+	const raw = await readFile('./data/players.json', { encoding: 'utf-8' });
+	const json = JSON.parse(raw);
+	const parsed = PlayersFileSchema.parse(json);
+	return parsed;
+};
