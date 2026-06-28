2f3f09b488ba50cd723768941e7a54faf23985db
Zachary Stence <zachstence@gmail.com>
2026-06-27 20:03:50 -0500

    fix: preload font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,11 +7,13 @@
 	import type { LayoutProps } from './$types';
 	import * as Tooltip from '$lib/components/ui/tooltip';
 	import { ModeWatcher } from 'mode-watcher';
+	import inter from '@fontsource-variable/inter?url';
 
 	let { children, data }: LayoutProps = $props();
 </script>
 
 <svelte:head>
+	<link rel="preload" as="font" type="font/woff2" href={inter} crossorigin="anonymous" />
 	<link rel="icon" type="image/svg+xml" href={faviconBlack} media="(prefers-color-scheme: light)" />
 	<link rel="icon" type="image/svg+xml" href={faviconWhite} media="(prefers-color-scheme: dark)" />
 	<title>ZenAI Poker Dashboard</title>
